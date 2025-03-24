dd2c18e5698ead3ca4f893e65f475d7955879cee
KOM(L_J) <2035543911@qq.com>
2025-03-24 17:40:30 +0800

    Initial commit

---
+++ parahomework1/para1/Inner/normal/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+#define n 5000
+int a[n];
+int b[n][n];
+int sum[n];
+
+int main()
+{
+    for(int i=0;i<n;i++){
+        a[i]=i;
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            b[i][j]=i+j;
+        }
+    }
+
+    //in normal way:
+
+    for(int i=0;i<n;i++){
+        sum[i]=0;
+        for(int j=0;j<n;j++){
+            sum[i]+=b[j][i]*a[j];
+        }
+    }
+
+
+    return 0;
+}
