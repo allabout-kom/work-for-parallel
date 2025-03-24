a5881720c9c728bbbd983800c175ee5c8cb26ec1
KOM(L_J) <2035543911@qq.com>
2025-03-24 19:43:15 +0800

    从 Git 记录中删除 try 目录，并忽略它

---
+++ parahomework1/para1/Sum/normal/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+#define maxN 10000000//10 billion
+int a[maxN];
+int sum=0;
+
+int main()
+{
+    int t=50;//50 rounds
+    for(int i=0;i<maxN;i++)
+    {
+        a[i]=i;
+    }
+
+    //in normal way
+    while(t--){
+        for(int i=0;i<maxN;i++)
+            sum+=a[i];
+    }
+    return 0;
+}
