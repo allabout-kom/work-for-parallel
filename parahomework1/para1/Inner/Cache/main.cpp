476b9c014c2e3aba1010641363ca50f1da4e45c8
KOM(L_J) <2035543911@qq.com>
2025-03-24 18:01:41 +0800

    Initial commit

---
+++ parahomework1/para1/Inner/Cache/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+#define n 5000
+int a[n];
+int b[n][n];
+int sum[n];
+
+int main()
+{
+    for(int i=0;i<n;i++){
+        a[i]=i;
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            b[i][j]=i+j;
+        }
+    }
+
+
+    for(int i=0;i<n;i++)
+        sum[i]=0;
+//differences:mainly decided by columns
+    for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++)
+        {
+            sum[i]+=a[j]*b[j][i];
+        }
+    }
+
+
+    return 0;
+}
