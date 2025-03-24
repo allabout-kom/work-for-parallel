38e4365f57b235d1afedc41d0625452ff715a5ac
KOM(L_J) <2035543911@qq.com>
2025-03-24 19:59:46 +0800

    加上加速

---
+++ parahomework1/para1/Sum/excess/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+#define maxN 10000000
+int a[maxN];
+int sum,sum1=0,sum2=0;
+
+int main()
+{
+    int t=50;
+    for(int i=0;i<maxN;i++)
+    {
+        a[i]=i;
+    }
+
+    //split it into two tasks
+    while(t--){
+        for(int i=0;i<maxN;i+=2){
+            sum1+=a[i];
+            sum2+=a[i+1];
+        }
+    }
+    sum=sum1+sum2;
+
+    return 0;
+}
